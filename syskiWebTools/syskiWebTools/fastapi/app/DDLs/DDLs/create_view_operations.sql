c1c518c6a25fd1159ad2fa3f6490cb6be1d2da77
Hiroki Furuya <htc4iztd333@outlook.jp>
2024-05-10 04:38:49 +0900

    12

--- syskiWebTools/syskiWebTools/fastapi/app/DDLs/DDLs/create_view_operations.sql
+++ syskiWebTools/syskiWebTools/fastapi/app/DDLs/DDLs/create_view_operations.sql
@@ -3,40 +3,62 @@ WITH CalculatedColumns AS (
     SELECT
         t1.seq_no,
         t1.issue_name,
-        t1.incharge_team,
+        t1.incharge_team AS issue_team,
         t1.incharge_person,
-        t1.changed_ope_manhours,
+        t1.act_ope_manhours,
         t2.issue_consideration_status,
-        COALESCE(CASE WHEN t2.issue_consideration_status = '要件定義中' THEN CAST((SELECT mst_value FROM general_mst WHERE category_name = '適切なカテゴリID' AND category_id = 1) AS INTEGER) ELSE 0 END, 0) AS issue_consideration_inprogress_manhours,
-        COALESCE(CASE WHEN t2.issue_consideration_status = '要件定義中' THEN CAST((SELECT mst_value FROM general_mst WHERE category_name = '課題残数' AND category_id = 2) AS INTEGER) ELSE 0 END, 0) AS correspond_manhours,
-        COALESCE(CASE WHEN t2.issue_consideration_status = '要件定義中' THEN CAST((SELECT mst_value FROM general_mst WHERE category_name = '想定打ち合わせ時間' AND category_id = 3) AS INTEGER) ELSE 0 END, 0) AS estimated_mtg_inprogress_manhours,
-        COALESCE(CASE WHEN t2.issue_consideration_status = '要件定義中' THEN CAST((SELECT mst_value FROM general_mst WHERE category_name = 'その他作業時間' AND category_id = 4) AS INTEGER) ELSE 0 END, 0) AS other_work_inprogress_manhours
+        COALESCE(
+          CASE
+            WHEN t2.issue_consideration_status = '要件定義中' AND t1.act_ope_manhours = '想定不可' THEN CAST((SELECT mst_value FROM general_mst WHERE id = 6) AS INTEGER)
+            WHEN t2.issue_consideration_status = '要件定義中' AND t1.act_ope_manhours = '特大' THEN CAST((SELECT mst_value FROM general_mst WHERE id = 7) AS INTEGER)
+            WHEN t2.issue_consideration_status = '要件定義中' AND t1.act_ope_manhours = '大' THEN CAST((SELECT mst_value FROM general_mst WHERE id = 8) AS INTEGER)
+            WHEN t2.issue_consideration_status = '要件定義中' AND t1.act_ope_manhours = '中' THEN CAST((SELECT mst_value FROM general_mst WHERE id = 9) AS INTEGER)
+            WHEN t2.issue_consideration_status = '要件定義中' AND t1.act_ope_manhours = '小' THEN CAST((SELECT mst_value FROM general_mst WHERE id = 10) AS INTEGER)
+            WHEN t2.issue_consideration_status = '要件定義中' AND t1.act_ope_manhours = '極小' THEN CAST((SELECT mst_value FROM general_mst WHERE id = 11) AS INTEGER)
+            ELSE 0
+          END, 0
+        ) AS issue_consideration_inprogress_manhours,
+        t2.estimated_mtg_last2wk_time,
+        t2.other_work_last2wk_time,
+        t2.remarks
     FROM
         issues t1
     JOIN
         operations_manual_input t2 ON t1.seq_no = t2.seq_no
 ),
-Counts AS (
+syskiCounts AS (
     SELECT
-        t1.seq_no AS seq_no,
-        COUNT(*) AS syski_task_remain,
-        t3.quarters || t3.issues_type || '(' || t3.issues_seq_no::text || ')' AS combined_column
+        t3.issues_seq_no AS seq_no,
+        COUNT(*) AS syski_task_remain
     FROM
         task t3
-    JOIN
-        issues t1 ON t1.seq_no = t3.issues_seq_no
-    WHERE
-        t3.incharge IN (SELECT mst_value FROM general_mst WHERE category_name = '業務設計担当T')
-    GROUP BY combined_column, t1.seq_no
+    GROUP BY t3.issues_seq_no
+),
+allCounts AS (
+    SELECT
+        t3.issues_seq_no AS seq_no,
+        COUNT(*) AS all_task_remain
+    FROM
+        task t3
+    GROUP BY t3.issues_seq_no
 )
 SELECT
-    cc.*,
-    co.syski_task_remain,
-    (COALESCE(cc.issue_consideration_inprogress_manhours, 0) + COALESCE(cc.correspond_manhours, 0) + COALESCE(cc.estimated_mtg_inprogress_manhours, 0) + COALESCE(cc.other_work_inprogress_manhours, 0)) AS total_manhours,
-    op.remarks
+    cc.seq_no,
+    cc.issue_name,
+    cc.issue_team,
+    cc.incharge_person,
+    cc.act_ope_manhours,
+    cc.issue_consideration_status,
+    cc.issue_consideration_inprogress_manhours,
+    sc.syski_task_remain,
+    ac.all_task_remain,
+    cc.estimated_mtg_last2wk_time,
+    cc.other_work_last2wk_time,
+    cc.remarks
 FROM
     CalculatedColumns cc
-JOIN
-    Counts co ON cc.seq_no = co.seq_no
-JOIN
-    operations_manual_input op ON cc.seq_no = op.seq_no;
+LEFT JOIN
+    syskiCounts sc ON cc.seq_no = sc.seq_no
+LEFT JOIN
+    allCounts ac ON cc.seq_no = ac.seq_no
+;
